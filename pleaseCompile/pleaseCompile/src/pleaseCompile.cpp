153b9c9cfdb5ca4b85e3194b029c8bf7c1712a83
NDPeshev19 <NDPeshev19@codingburgas.bg>
2021-11-06 15:27:50 +0200

    Draw optimizations

--- pleaseCompile/pleaseCompile/src/pleaseCompile.cpp
+++ pleaseCompile/pleaseCompile/src/pleaseCompile.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include "maze.h"
 #include "olcConsoleGameEngine.h"
 
@@ -12,6 +13,13 @@ public:
     float PlayerX = 0;
     float PlayerY = 0;
     float PlayerSpeed = 30;
+    struct coords
+    {
+        int x;
+        int y;
+    };
+
+    std::vector< std::pair<coords, coords> > wallCoords;
 
     void DrawRectangle(int x1, int y1, int x2, int y2, short c) {
         DrawLine(x1, y1, x1, y2, PIXEL_SOLID, c);
@@ -42,26 +50,26 @@ public:
         if (PlayerDirection == "left") {
             PlayerX -= (float)PlayerSpeed * fElapsedTime;
         }
-        for (auto cell : maze.grid)
-        {
-            if (cell->walls[Maze::CellWallDirs::top]) {
-                DrawLine(cell->x * 20, cell->y * 20, cell->x * 20 + 20, cell->y * 20, FG_BLACK);
-            }
-            if (cell->walls[Maze::CellWallDirs::right]) {
-                DrawLine(cell->x * 20 + 20, cell->y * 20, cell->x * 20 + 20, cell->y * 20 + 20, FG_BLACK);
-            }
-            if (cell->walls[Maze::CellWallDirs::bottom]) {
-                DrawLine(cell->x * 20, cell->y * 20 + 20, cell->x * 20 + 20, cell->y * 20 + 20, FG_BLACK);
-            }
-            if (cell->walls[Maze::CellWallDirs::left]) {
-                DrawLine(cell->x * 20 + 20, cell->y * 20 + 20, cell->x * 20 + 20, cell->y * 20 + 20, FG_BLACK);
-            }
-        }
-
     }
    
     Maze maze;
     virtual bool OnUserCreate() {
+        for (auto cell : maze.grid)
+        {
+            if (cell->walls[Maze::CellWallDirs::top]) {
+                this->wallCoords.push_back({ {cell->x * 20, cell->y * 20}, {cell->x * 20 + 20, cell->y * 20} });
+            }
+            if (cell->walls[Maze::CellWallDirs::right]) {
+                this->wallCoords.push_back({ {cell->x * 20 + 20, cell->y * 20}, {cell->x * 20 + 20, cell->y * 20 + 20} });
+            }
+            if (cell->walls[Maze::CellWallDirs::bottom]) {
+            //    this->wallCoords.push_back({ {cell->x * 20, cell->y * 20 + 20}, {cell->x * 20 + 20, cell->y * 20 + 20} });
+            }
+            if (cell->walls[Maze::CellWallDirs::left]) {
+            //    this->wallCoords.push_back({ {cell->x * 20, cell->y * 20}, {cell->x * 20, cell->y * 20 + 20} });
+            }
+        }
+
         return 1;
     }
     
@@ -70,6 +78,12 @@ public:
      Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_GREEN);
 
      DrawRectangle(PlayerX, PlayerY, PlayerX + 10, PlayerY + 10, FG_BLACK);
+
+     for (auto wall : this->wallCoords)
+     {
+         DrawLine(wall.first.x, wall.first.y, wall.second.x, wall.second.y, FG_BLACK);
+     }
+
      MovePlayer(fElapsedTime);
         return 1;
     }
